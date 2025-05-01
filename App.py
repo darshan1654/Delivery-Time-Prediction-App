3a29e7d56d692b4c6f616f1e0ed20ba84caed66d
Darshan Pardeshi <128826296+darshan1654@users.noreply.github.com>
2025-05-01 23:16:40 +0530

    Upload App.py

--- App.py
+++ App.py
@@ -6,22 +6,24 @@ from PIL import Image
 import time
 
 # Load trained model
-with open("models/delivery_time_model.pkl", "rb") as f:
+with open("models/delivery_time_n_model.pkl", "rb") as f:
     model_data = pickle.load(f)
     model = model_data["model"]
     feature_columns = model_data["features"]
 
-# App Title and Caption
+# App Header Image
 img = Image.open("./assets/delivery_app.png")
 img_resized = img.resize((380, 150))
 st.image(img_resized)
+
+# Title and Description
 st.title("📦 Delivery Time Prediction App ")
 st.caption("This is a Streamlit-based web application that predicts delivery time for an order based on multiple input features like shipping method, customer location, weather, distance, and more. The prediction model is trained using Python's scikit-learn library and serialized using pickle.")
 
-# UI Inputs
+# Sidebar Input Fields
 with st.sidebar:
-    # Input Parameters
-    st.header("Input Parameters")
+    st.header("👉 Input Parameters")
+
     product_category = st.selectbox("🛍️ ***Product Category***", ["Electronics", "Clothing", "Home & Kitchen", "Books", "Other"])
     customer_location = st.selectbox("📍 ***Customer Location***", ["Urban", "Suburban", "Rural"])
     shipping_method = st.selectbox("🚚 ***Shipping Method***", ["Standard", "Express", "Same-Day"])
@@ -32,13 +34,14 @@ with st.sidebar:
     distance = st.number_input("📍 ***Distance to Destination (km)***", min_value=1, max_value=5000, step=1)
     warehouse_available = st.radio("🏢 ***Available in Nearby Warehouse?***", ["Yes", "No"])
     delivery_type = st.selectbox("🏠 ***Delivery Type***", ["Residential", "Commercial"])
+
     submit = st.button(label="**🚀 Predict Delivery Time**")
 
+# Output Section
 with st.container():
-    # Predict Button
     if submit:
-        st.subheader("💡Output: Delivery Time in Days")
-        # Create a DataFrame for the single input
+        st.subheader("📊 Predicted Delivery Time")
+
         input_dict = {
             "weight": [weight],
             "distance": [distance],
@@ -54,13 +57,11 @@ with st.container():
 
         input_df = pd.DataFrame(input_dict)
 
-        # Encode using the same dummy structure as training
+        # Encode input to match training format
         input_encoded = pd.get_dummies(input_df)
-
-        # Align with training feature columns
         input_encoded = input_encoded.reindex(columns=feature_columns, fill_value=0)
         
-        # Predict
+        # Prediction
         prediction = model.predict(input_encoded)[0]
         prediction = round(prediction)
 
@@ -68,5 +69,6 @@ with st.container():
             time.sleep(1)
             st.success(f"📦 ***Estimated Delivery Time***:  **{prediction} days**")
 
+# Footer
 st.markdown("---")
 st.markdown("👨‍💻 Developed by [Darshan Pardeshi](https://github.com/darshan1654/Delivery-Time-Prediction-App)")
